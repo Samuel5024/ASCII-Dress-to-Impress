56b550e2c1f24b729b44010039b67bea12b3c03a
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-03-28 18:33:16 -0700

    Update femaleModel.cpp

--- Woman/femaleModel/femaleModel.cpp
+++ Woman/femaleModel/femaleModel.cpp
@@ -1,11 +1,11 @@
 #include "femaleModel.h"
 
 void displayFModel() {
-    fstream female("female.txt");
-    string model;
-    for(int i = 0; i < 144; i++) {
-        getline(female, model);
-        cout << model << endl;
-    }
-    female.close();
+     fstream female("female.txt");
+ string model;
+
+while(getline(female, model)){
+     cout << model << endl;
+ }
+ 
 }
