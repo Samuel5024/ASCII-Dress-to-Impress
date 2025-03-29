4288a2f71438b795ebdd183045f1bc3ed4d32b25
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-03-28 18:36:17 -0700

    Update blueBlouse.cpp

--- Woman/blouse/blueBlouse/blueBlouse.cpp
+++ Woman/blouse/blueBlouse/blueBlouse.cpp
@@ -1,9 +1,10 @@
 #include "blueBlouse.h"
 
 void displayBlueBlousePattern() {
-    fstream blouse("blueBlouse.txt");
+    fstream blouse ("blueBlouse.txt");
     string blueBlouse;
-    while(getline(blouse, blueBlouse)) {
+    
+    while (getline(blouse, blueBlouse)) {
         cout << "\033[34m" << blueBlouse << endl;
     }
     
