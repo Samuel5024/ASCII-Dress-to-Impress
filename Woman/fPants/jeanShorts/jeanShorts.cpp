d87d8c332d77e037650bb6dd49d4549579ddb975
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-04-01 19:06:56 -0700

    Update jeanShorts.cpp

--- Woman/fPants/jeanShorts/jeanShorts.cpp
+++ Woman/fPants/jeanShorts/jeanShorts.cpp
@@ -6,21 +6,8 @@ void displayJeanShortsPattern() {
     int row = 0;
     
     while(getline(pants, jeanShorts)) {
-        int width = jeanShorts.length();
         
-        for(size_t col = 0; col < width; col++) {
-            
-            if(row >= 1 && row <= 2){
-                
-               cout << "\\033[30m" << jeanShorts[col];
-            } 
-            
-            else{
-                cout << "\\033[34m" << jeanShorts[col];
-            }
-        }
-        cout << "\\033[0m" << endl;
-        row++;        
+        court << jeanShorts << endl;        
     }
     
     pants.close();
