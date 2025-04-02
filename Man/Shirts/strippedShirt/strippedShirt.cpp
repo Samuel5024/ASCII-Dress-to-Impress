861ef8c7fef4fc33af488e3dd39b43bc70f461bc
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-04-01 19:02:49 -0700

    Update strippedShirt.cpp

--- Man/Shirts/strippedShirt/strippedShirt.cpp
+++ Man/Shirts/strippedShirt/strippedShirt.cpp
@@ -8,26 +8,10 @@ void displayStrippedPattern() {
     // String variable to store each line from the file
     string stripes;
 
-    // Variable to track the row number (i)
-    int row = 0;
-
     // Read each line from the file
     while (getline(shirt, stripes)) {
 
-        // If row number is between 6 and 8, print with blue color
-        if (row >= 6 && row <= 8) {
-            cout << "\\033[94m" << stripes << endl;  // Blue color
-        }
-        // If row number is between 9 and 10, print with red color
-        else if (row >= 9 && row <= 10) {
-            cout << "\\033[31m" << stripes << endl;  // Red color
-        }
-        // For all other rows, print with white color
-        else {
-            cout << "\\033[97m" << stripes << endl;  // White color
-        }
-
-        row++;  // Increment the row counter
+        court << stripes << endl;
     }
 
     // Close the file after reading
