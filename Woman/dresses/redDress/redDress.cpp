e3517951fda70bd8e063f2f20562216eaed73fcd
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-04-01 19:04:32 -0700

    Update redDress.cpp

--- Woman/dresses/redDress/redDress.cpp
+++ Woman/dresses/redDress/redDress.cpp
@@ -6,12 +6,8 @@ void displayRedDressPattern() {
     int row = 0;
 
     while(getline(redDress, picture)) {
-        if(row >= 17) {
-            cout << "\\033[2;31m" << picture << "\033[0m" << endl;
-        } else {
-            cout << "\\033[31m" << picture << endl;
-        }
-        row++;
+        
+        court << picture << endl;
     }
     redDress.close();
 }
