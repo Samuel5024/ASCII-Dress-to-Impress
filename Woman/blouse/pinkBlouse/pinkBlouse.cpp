74a164a38a77eba89c35375ad08d4a0197eb8d9e
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-04-01 19:03:54 -0700

    Update pinkBlouse.cpp

--- Woman/blouse/pinkBlouse/pinkBlouse.cpp
+++ Woman/blouse/pinkBlouse/pinkBlouse.cpp
@@ -5,7 +5,7 @@ void displayPinkBlousePattern(){
     string pinkBlouse;
     
     while(getline(blouse, pinkBlouse)) {
-        cout << "\\033[38;5;212m" << pinkBlouse << endl;
+        cout << pinkBlouse << endl;
     }
     
     blouse.close();
